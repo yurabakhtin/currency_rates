da9f5ad28a751bf8cb28c443876fc658e54a2404
yurabakhtin <yurybakh@gmail.com>
2020-11-29 23:20:21 +0300

    Doc

--- app/Facades/ImportCurrency.php
+++ app/Facades/ImportCurrency.php
@@ -3,6 +3,14 @@ namespace App\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static bool|array run(string $source, $logger = null)
+ * @method static bool|array getXmlData($source)
+ * @method static \App\Models\CurrencyRate|string importRate($currencyXmlElement, $date)
+ * @method static string getFormattedDate($xmlElement, $sourceFormat = 'd.m.Y', $targetFormat = 'Y-m-d')
+ *
+ * @see \App\Services\XmlImportCurrencyService
+ */
 class ImportCurrency extends Facade
 {
     protected static function getFacadeAccessor()
--- app/Facades/Token.php
+++ app/Facades/Token.php
@@ -3,6 +3,15 @@ namespace App\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \App\Services\ApiTokenService user(\App\Models\User $user)
+ * @method static string getNew(int $length = 60)
+ * @method static void delete()
+ * @method static void save(string $token)
+ * @method static string hash(string $token)
+ *
+ * @see \App\Services\ApiTokenService
+ */
 class Token extends Facade
 {
     protected static function getFacadeAccessor()
